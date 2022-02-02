9614a96a86a9a09ee2dedd72b642b2eea71bdaf8
fst <fst_werk@outlook.es>
2022-02-02 11:20:20 +0200

    new branch for api module

--- get_api.py
+++ get_api.py
@@ -1,1 +1,6 @@
-#translate py
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.translation.document import DocumentTranslationClient
+
+endpoint = "https://<resource-name>.cognitiveservices.azure.com/"
+credential = AzureKeyCredential("<api_key>")
+document_translation_client = DocumentTranslationClient(endpoint, credential)
