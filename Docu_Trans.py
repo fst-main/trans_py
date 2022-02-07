9c5581bbbfeb7010da2c01b91c24a5e426ed03f8
fst <fst_werk@outlook.es>
2022-02-07 11:02:35 +0200

    Code syntasis corrected

--- Docu_Trans.py
+++ Docu_Trans.py
@@ -4,7 +4,7 @@ from azure.ai.translation.document import DocumentTranslationClient
 
 # Add your subscription key and endpoint
 subscription_key = "c34d7e1ff84d4a35855631f70558051f"
-endpoint = "https://api.cognitive.microsofttranslator.com"
+endpoint = "https://siitranspy.cognitiveservices.azure.com/"
 
 # Add your location, also known as region. The default is global.
 # This is required if using a Cognitive Services resource.
@@ -14,18 +14,19 @@ path = '/translate'
 constructed_url = endpoint + path
 
 params = {
-    'api-version': '3.0'
-    'from':'pl'
-    'to':'en'
+    'api-version': '3.0',
+    'from':'en',
+    'to':'es'
 }
+
 constructed_url = endpoint + path
 
 headers = {
-    'Ocp-Apim-Subscription-Key': subscription_key
-    'Ocp-Apim-Subscription-Region': location
-    'Content-type': 'application/json'
+    'Ocp-Apim-Subscription-Key': 'subscription_key',
+    'Ocp-Apim-Subscription-Region': 'location',
+    'Content-type': 'application/json',
     'X-ClientTraceId': str(uuid.uuid4())
-}
+    }
 
 # You can pass more than one object in body.
 body = [{
@@ -39,4 +40,4 @@ print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separat
 
 
 document_translation_client = DocumentTranslationClient("https://api.cognitive.microsofttranslator.com", AzureKeyCredential("c34d7e1ff84d4a35855631f70558051f"))
-poller = document_translation_client.begin_translation("https://blobish1.blob.core.windows.net/sas1", "https://blobish1.blob.core.windows.net/sas2", "en")
+poller = document_translation_client.begin_translation("https://blobish1.blob.core.windows.net/sas1/polish.txt?sp=r&st=2022-02-03T15:05:45Z&se=2022-02-03T23:05:45Z&spr=https&sv=2020-08-04&sr=b&sig=LixkVsfnV49KzCmObJ3eWhmkDg00u%2BqkqAW%2FNpbiY6M%3D", "https://blobish1.blob.core.windows.net/sas2/tradus.txt?sp=r&st=2022-02-03T15:07:30Z&se=2022-02-03T23:07:30Z&spr=https&sv=2020-08-04&sr=b&sig=PCXO7eKd%2FPtPlUPT1JFpKEvMc24zxkXl6Vzn4X5XDYw%3D", "en")
